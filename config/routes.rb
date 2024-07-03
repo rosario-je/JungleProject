70fd582cafde0b53f00d0d31d5483be5bbe3a3b9
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-02 22:51:18 -0400

    feat: Add categories route to admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,13 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create, :destroy]
   end
 
   get 'about', to: 'about#index'
+
+
+  
 
 
   # The priority is based upon order of creation: first created -> highest priority.
