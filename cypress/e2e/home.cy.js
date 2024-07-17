c38037cb13810bcf8d102206d0494a052e4e0cea
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-17 02:14:01 -0400

    chore: Add Cypress e2e tests for home page navigation and product display

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,19 @@
+
+describe("Navigation", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/")
+  })
+  
+  it ("visits Jungle App", () => {
+    cy.visit("http://localhost:3000/")
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  });
+
+})
